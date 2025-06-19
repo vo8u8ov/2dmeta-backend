1c0b0760d0ddaefd9c26b9c478e9c0a63da49177
Mariko.Sakai <28652441+vo8u8ov@users.noreply.github.com>
2025-06-19 11:50:27 +0900

    調整

--- config/runtime.exs
+++ config/runtime.exs
@@ -39,7 +39,7 @@ if config_env() == :prod do
   config :backend, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :backend, BackendWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
